9d72b4381e84f7ece0139ad66b6478c027a67315
ali-salhab <alysalhab5@gmail.com>
2026-07-18 07:22:50 +0200

    swqe

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -5,7 +5,7 @@ import {
   syncUserUpdate,
   syncUserDeletion,
 } from "@/config/inngest";
-
+export const dynamic = "force-dynamic";
 console.log("route file --------------->");
 export const { GET, POST, PUT } = serve({
   client: inngest,
